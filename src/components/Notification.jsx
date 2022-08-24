05be81241cfad53826a13bcc85cd41745b4138e3
G Nitin <nitin.naik@rayabharitech.com>
2022-08-24 18:56:33 +0530

    Implemented functionality of notification model

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,28 +1,52 @@
-import React from 'react'
 import NotificationLayout from '../layouts/NotificationLayout'
 import { Button } from 'antd'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import NotificationAlert from './NotificationAlert'
+import { hideNotificationAlert } from '../redux/reducers/notificationReducer'
 
-const ButtonList = [
+const actionButtonList = [
   { key: 1, title: 'success' },
   { key: 2, title: 'Error' },
   { key: 3, title: 'warning' },
   { key: 4, title: 'info' },
-  { key: 5, title: 'unstyled' },
+  { key: 5, title: 'default' },
 ]
 
 const Notification = () => {
   const { notificationList } = useSelector(state => state.notification)
-  const ButtonElement = ButtonList.map(button => (
-    <Button className="components-notification__action-btn" key={button.key} size="large">
-      Hide {button.title}
+
+  const dispatch = useDispatch()
+
+  const onClose = selectedNotification => dispatch(hideNotificationAlert(selectedNotification))
+
+  const ActionButtons = actionButtonList.map((button, index) => (
+    <Button
+      className="components-notification__action-btn"
+      key={button.key}
+      size="large"
+      onClick={() => onClose(notificationList[index].variant)}
+    >
+      {notificationList[index].isHidden ? 'Show' : 'Hide'} {button.title}
     </Button>
+  ))
+
+  const NotificationAlerts = notificationList.map(notification => (
+    <NotificationAlert
+      key={notification.key}
+      text={notification.text}
+      variant={notification.variant}
+      timeout={notification.timeout}
+      Icon={notification.icon}
+      isHidden={notification.isHidden}
+      onClose={() => onClose(notification.variant)}
+      onTimer={() => onClose(notification.variant)}
+    />
   ))
 
   return (
     <div className="components-notification">
-      <div className="components-notification__btn-wrapper">{ButtonElement}</div>
-      <div className="components-notification__alert-wrapper"></div>
+      <div className="components-notification__btn-wrapper">{ActionButtons}</div>
+      <div className="components-notification__alert-wrapper">{NotificationAlerts}</div>
     </div>
   )
 }
---
+++ src/components/NotificationAlert.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import { CloseOutlined } from '@ant-design/icons'
+
+const NotificationAlert = ({
+  text = '',
+  variant = '',
+  timeout = 0,
+  Icon = null,
+  isHidden = false,
+  onClose = () => {},
+  onTimer = () => {},
+}) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onTimer()
+    }, timeout)
+    return () => {
+      timer && clearTimeout(timer)
+    }
+  }, [])
+
+  return (
+    <div className={`component-notification-alert ${variant} ${isHidden ? 'hide-alert' : 'open'}`}>
+      <div className="component-notification-alert__icon-wrapper">
+        {Icon && <Icon className="component-notification-alert__icon" />}
+        <span className="component-notification-alert__text">{text}</span>
+      </div>
+      <CloseOutlined className="component-notification-alert__close-icon" onClick={onClose} />
+    </div>
+  )
+}
+
+export default NotificationAlert
